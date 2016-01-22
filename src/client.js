379b1234d45463743815a2878dca15c0b5044f07
Cameron Little <cameron@camlittle.com>
2016-01-21 21:58:03 -0800

    Fixed some stuff for running on the pi.

--- src/client.js
+++ src/client.js
@@ -1,9 +1,12 @@
+require('babel-polyfill');
+
 var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainComponent = require('./client/components/mainComponent');
 
+// make global to make debugging easier
 window.$ = $;
 
 ReactDOM.render((
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var calendar = require('./server/api/calendar');
+// var calendar = require('./server/api/calendar');
 var weather = require('./server/api/weather');
 var ticker = require('./server/api/ticker');
 
 var app = express();
 
-var port = 8001;
+var port = 8101;
 
 app.use(express.static('./dst'));
 
@@ -16,7 +16,7 @@ app.get('/api', function(req, res) {
 });
 app.get('/api/weather/:lat/:lng', weather.get);
 app.get('/api/ticker', ticker.get);
-app.get('/api/calendar/events', calendar.get);
+// app.get('/api/calendar/events', calendar.get);
 
 app.listen(port);
 console.log(`Listening on port ${port}`);

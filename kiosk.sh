1ac1fa5378bb286e257a04164b4eb53363e7d27b
Cameron Little <cameron@camlittle.com>
2016-02-07 16:44:36 -0800

    Switched back to chromium

--- kiosk.sh
+++ kiosk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#chromium-browser --kiosk http://localhost:8101 --noerrdialogs --incognito
-iceweasel -url http://localhost:8101 -foreground -fullscreen
+chromium-browser --kiosk http://localhost:8101 --noerrdialogs --incognito
+#iceweasel -url http://localhost:8101 -foreground -fullscreen

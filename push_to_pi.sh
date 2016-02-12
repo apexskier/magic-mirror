72be1f85d9a1bd652dafae23c7c1ed2e95f3eef1
Cameron Little <cameron@camlittle.com>
2016-02-12 12:42:52 -0800

    openbox - Added autostart. BG black

--- push_to_pi.sh
+++
@@ -1,7 +0,0 @@
-./node_modules/.bin/gulp build
-
-ssh pi@10.0.1.3 "mkdir -p magic-mirror"
-
-rsync -avz --exclude node_modules --progress . pi@10.0.1.3:magic-mirror
-
-ssh pi@10.0.1.3 "cd magic-mirror && npm install --production && sudo service magic-mirror restart"
---
+++ startup.sh
@@ -0,0 +1,11 @@
+NAME="magic-mirror"
+cd magic-mirror
+tmux new-session -s "$NAME" -d
+tmux send-keys -t "$NAME" "node src/server.js" Enter
+tmux split-window -h -t "$NAME"
+tmux send-keys -t "$NAME" "python vision/main.py" Enter
+tmux new-window -a -t "$NAME"
+tmux send-keys -t "$NAME" "./kiosk.sh" Enter
+tmux split-window -h -t "$NAME"
+tmux select-window -l -t "$NAME"
+tmux -2 attach-session -t "$NAME"

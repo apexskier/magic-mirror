1d528d8528b53ee5b70e8608b8dd5b15dea20165
Cameron Little <cameron@camlittle.com>
2016-02-13 19:29:38 -0800

    Cleanup

---
+++ src/private.example.js
@@ -0,0 +1,8 @@
+exports.forecast = 'APIKEY';
+
+exports.twitter = {
+    consumer_key: 'PUBLICKEY',
+    consumer_secret: 'PRIVATEKEY',
+    access_token_key: 'PUBLICKEY',
+    access_token_secret: 'PRIVATEKEY'
+};
